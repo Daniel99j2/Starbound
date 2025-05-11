92ee9392cc9f976bd33c0b10b0c8ad8b8fa5e262
Daniel99j2 <daniel99j1235@proton.me>
2025-05-11 12:19:18 +0800

    Initial commit

---
+++ src/main/java/com/danie99j/starbound/StarboundDataGenerator.java
@@ -0,0 +1,11 @@
+package com.danie99j.starbound;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StarboundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
